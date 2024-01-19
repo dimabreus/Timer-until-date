4d580ca7088b16d7a566036b573a3f4e308dea4a
dimabreus <dmitriybreus5@gmail.com>
2024-01-19 15:33:18 +0300

    Update code for countdown timer to display days, hours, minutes, seconds, and milliseconds in Russian.

--- main.js
+++ main.js
@@ -1,32 +1,111 @@
-const date = new Date(2024, 0, 15, 20, 0, 0, 0);
+const output = document.querySelector("#output");
 
-const goodMilliSeconds = true;
+let date, inputDate, inputTime;
 
-setInterval(() => {
-    let nowDate = new Date();
+const dateCheck = (date) => {
+    inputDate = date.split("-").map(Number);
+    inputDate[1]--;
+    if (inputDate && inputTime) createDate();
+};
 
-    let dif = date - nowDate;
+const timeCheck = (time) => {
+    inputTime = time.split(":").map(Number);
+    if (inputDate && inputTime) createDate();
+};
 
-    let difMilliSeconds;
+const createDate = () => {
+    date = new Date(...inputDate, ...inputTime, 0, 0);
+    document.cookie = `inputDate=${btoa(inputDate)}`;
+    document.cookie = `inputTime=${btoa(inputTime)}`;
+    start();
+};
 
-    if (goodMilliSeconds === true){
+window.onload = () => {
+    if (document.cookie && document.cookie != "") {
+        document.querySelector("#date").value = atob(document.cookie.match(/(?:^|;) *inputDate=([^;]*)/)[1]).split(",").map((el, index) => index === 1 ? String(Number(el) + 1).padStart(2, '0') : (index === 0 ? el.padStart(4, '0') : el.padStart(2, '0'))).join("-");
+        document.querySelector("#time").value = atob(document.cookie.match(/(?:^|;) *inputTime=([^;]*)/)[1]).split(",").map(el => el.padStart(2, '0')).join(":");
+    }
+
+    if (document.querySelector("#date").value) dateCheck(document.querySelector("#date").value);
+    if (document.querySelector("#time").value) timeCheck(document.querySelector("#time").value);
+
+}
+
+const formatDays = count => {
+    if (count === 1) {
+        return "день";
+    } else if (count >= 2 && count <= 4) {
+        return "дня";
+    } else {
+        return "дней";
+    }
+}
+
+const formatHours = count => {
+    if (count === 1 || count === 21) {
+        return "час";
+    } else if ((count >= 2 && count <= 4) || (count >= 22 && count <= 24)) {
+        return "часа";
+    } else {
+        return "часов";
+    }
+}
+
+const formatMinutes = count => {
+    if (count === 1 || count === 21 || count === 31 || count === 41 || count === 51) {
+        return "минута";
+    } else if ((count >= 2 && count <= 4) || (count >= 22 && count <= 24) || (count >= 32 && count <= 34) || (count >= 42 && count <= 44) || (count >= 52 && count <= 54)) {
+        return "минуты";
+    } else {
+        return "минут";
+    }
+}
+
+const formatSeconds = count => {
+    if (count === 1 || count === 21 || count === 31 || count === 41 || count === 51) {
+        return "секунда";
+    } else if ((count >= 2 && count <= 4) || (count >= 22 && count <= 24) || (count >= 32 && count <= 34) || (count >= 42 && count <= 44) || (count >= 52 && count <= 54)) {
+        return "секунды";
+    } else {
+        return "секунд";
+    }
+}
+
+const start = () => {
+    setInterval(() => {
+        let currentDate = new Date();
+
+        let dif = date - currentDate;
+
+        if (dif < 0) {
+            output.textContent = "Время истекло";
+            return;
+        }
+
+        let difMilliSeconds;
+
         difMilliSeconds = +String(dif)[String(dif).length - 3];
-    } else{
-        difMilliSeconds = +String(dif)[String(dif).length - 3] + String(dif)[String(dif).length - 2] + String(dif)[String(dif).length - 1];
-    }
 
-    let difSeconds = +String(dif).slice(0, String(dif).length - 3);
 
-    let difMinutes = Math.floor(difSeconds / 60);
-    difSeconds = difSeconds - difMinutes * 60;
+        let difSeconds = +String(dif).slice(0, String(dif).length - 3);
 
-    let difHours = Math.floor(difMinutes / 60);
-    difMinutes = difMinutes - difHours * 60;
+        let difMinutes = Math.floor(difSeconds / 60);
+        difSeconds = difSeconds - difMinutes * 60;
 
-    if (goodMilliSeconds === true){
-        console.log(`${String(difHours).padStart(2, 0)}:${String(difMinutes).padStart(2, 0)}:${String(difSeconds).padStart(2, 0)}:${String(difMilliSeconds).padStart(1, 0)}`);
-    } else{
-        console.log(`${String(difHours).padStart(2, 0)}:${String(difMinutes).padStart(2, 0)}:${String(difSeconds).padStart(2, 0)}:${String(difMilliSeconds).padStart(3, 0)}`);
-    }
+        let difHours = Math.floor(difMinutes / 60);
+        difMinutes = difMinutes - difHours * 60;
 
-}, 10 ? goodMilliSeconds : 1);
+        let difDays = Math.floor(difHours / 24);
+        difHours = difHours - difDays * 24;
+
+
+        output.innerHTML = `
+            ${String(difDays)} ${formatDays(difDays)} <br/>
+            ${String(difHours).padStart(2, 0)} ${formatHours(difHours)} <br/>
+            ${String(difMinutes).padStart(2, 0)} ${formatMinutes(difMinutes)} <br/>
+            ${String(difSeconds).padStart(2, 0)} ${formatSeconds(difSeconds)} <br/>
+            ${String(difMilliSeconds).padStart(1, 0)} миллисекунд
+        `;
+
+    }, 10);
+};
